ec66cb3930426c19b224d0926ac7dbc7cdae0e93
Jason Hawkins <95847296+MrHawkins-Babington@users.noreply.github.com>
2022-02-07 09:04:18 +0000

    Add files via upload

---
+++ challenge 018.py
@@ -0,0 +1,11 @@
+#challenge 018
+
+num = int(input("Please enter a number.\n"))
+
+
+if num < 10:
+    print("Too low.")
+elif num >=10 and num <=20:
+    print("Thank you.")
+else:
+    print("Too high.")
---
+++ challenge 019.py
@@ -0,0 +1,13 @@
+#challenge 019
+
+num = int(input("Please enter a 1, 2 or 3.\n"))
+
+
+if num ==1:
+    print("Thank you.")
+elif num ==2:
+    print("Well done.")
+elif num ==3:
+    print("Correct.")
+else:
+    print("Error.")
