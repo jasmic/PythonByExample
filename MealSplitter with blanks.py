ce284780d412cbbc51b09123995c7a3ad44fe658
Jason Hawkins <95847296+MrHawkins-Babington@users.noreply.github.com>
2022-12-05 12:46:36 +0000

    Add files via upload

---
+++ MealSplitter with blanks.py
@@ -0,0 +1,20 @@
+#The Meal Price Splitter
+#This works out how much each person
+#needs to pay for a group meal
+
+#This code introduces the Meal Price Spliter
+print("...add text here...")
+
+#It then asks how many people were at the meal
+value1=input("...add text here...")
+people=...add data type here...(value1)
+
+#now we need to find out the total meal cost
+value2=input("...add text here...")
+bill=...add data type here...(value2)
+
+#now we find out how much each person pays
+priceEach=...add data type here...(what?/what?)
+print("...add text here..." + priceEach)
+
+
